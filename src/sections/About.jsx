6b07358aaa7f9f323a3c902928556d99aac93be4
Agustin Osatinsky <agustinosatinsky@hotmail.com>
2023-08-14 16:53:05 +0300

    about and edu section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import "./About.css";
 
 function About() {
   return (
-    <div>
-      <h2>About Me</h2>
-      <p>This is some information about me.</p>
+    <div className="about-container">
+      <div className="about-content">
+        <img src="/images/profile.jpg" alt="About Me" className="about-image" />
+        <div className="about-text">
+          <h2>About Me</h2>
+          <p>
+            Highly motivated Systems Engineer graduate with honors, dedicated to
+            contributing to team achievements through diligent work, attention
+            to detail, and organizational abilities. Skilled in Node.JS
+            development and proficient in Cloud technologies. Possesses
+            excellent teamworking and communication skills, with a passion for
+            discussing ideas and having an impact on the product.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import "./Education.css";
 
 function Education() {
   return (
-    <div>
-      <h2>Education</h2>
+    <div className="education-container">
+      <h2>Information Systems Engineering</h2>
+      <h5>Universidad Tecnológica Nacional</h5>
       <ul>
-        <li>BS in Computer Science from XYZ University.</li>
-        <li>Masters in Artificial Intelligence from ABC University.</li>
+        <li>
+          Completed a 5-year degree with honors, achieving the highest GPA (8.72
+          out of 10) of the entire university{" "}
+          <a href="http://www.frt.utn.edu.ar/index.php?s=noticia&id=2352">
+            (read more here)
+          </a>
+          . Developed a system using computer vision to detect firearms as the
+          final project of my degree.
+        </li>
       </ul>
     </div>
   );
