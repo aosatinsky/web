9a37fdec9e22a1bc99fc86306d8b40b0f24cf365
Agustin Osatinsky <agustinosatinsky@hotmail.com>
2023-08-15 02:02:52 +0300

    more info section

--- src/sections/MoreInfo.jsx
+++ src/sections/MoreInfo.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import "./MoreInfo.css";
 
 function MoreInfo() {
   return (
-    <div>
-      <h2>Contact me!</h2>
-      <p>This is some information about me.</p>
+    <div className="more-info-container">
+      <h2>Contact Me!</h2>
+      {/* Links to LinkedIn and GitHub */}
+      <div className="social-links">
+        <a
+          href="https://www.linkedin.com/in/aosatinsky/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/icons/linkedin.svg"
+            alt="LinkedIn"
+            className="social-icon"
+          />
+        </a>
+        <a
+          href="https://github.com/aosatinsky/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/icons/github.svg"
+            alt="GitHub"
+            className="social-icon"
+          />
+        </a>
+      </div>
+      <p>
+        <a href="mailto:osatinsky.tech@gmail.com">osatinsky.tech@gmail.com</a>
+      </p>
+      {/* <p>Location: </p> */}
+      {/* <p>Phone: </p> */}
+      <p>
+        If you're interested in working together or learning more about what I
+        do, don't hesitate to reach out!
+      </p>
+
+      <a target="_blank" href="https://icons8.com">
+        icons provided by Icons8
+      </a>
     </div>
   );
 }
